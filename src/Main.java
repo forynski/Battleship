fd3f744ad20005570840dd2889b2bb2ee1074e43
forynski <65871510+forynski@users.noreply.github.com>
2021-03-04 16:43:12 +0000

    Preparing field

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,18 @@
+package battleship;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("  ");
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        char c;
+        for (c = 'A'; c <= 'H'; c++) {
+            System.out.println(c + " ");
+            
+        }
+    }
+}
 
