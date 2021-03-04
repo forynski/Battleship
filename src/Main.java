c759430b454f744b8352665778b47809b94e5318
forynski <65871510+forynski@users.noreply.github.com>
2021-03-04 16:52:57 +0000

    Filling field first stage

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
 package battleship;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.print("  ");
-        for (int i = 1; i <= 10 ; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
         char c;
         for (c = 'A'; c <= 'H'; c++) {
             System.out.println(c + " ");
-            
+        }
+
+        int rows = 10;
+        int columns = 10;
+        char[][] charArray = new char[rows][columns];
+
+        for (char[] chars : charArray) {
+            Arrays.fill(chars, '~');
+            System.out.println(chars);
         }
     }
 }
-
