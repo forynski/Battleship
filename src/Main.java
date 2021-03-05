476dca7d97f032f73b84a891d951db9977e7e50e
forynski <65871510+forynski@users.noreply.github.com>
2021-03-05 14:06:11 +0000

    stage 1/5 check coordinates

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,32 @@
 package battleship;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         String[] numbers = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
         String[] letters = {" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 
-        for (int i = 0 ; i <= 10 ; i++){
-            for (int j = 0 ; j <= 10 ; j++){
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j <= 10; j++) {
                 if (i == 0 || j == 0) {
                     System.out.print(i == 0 ? numbers[j] : letters[i]);
-                }
-                else System.out.print("~");
+                } else System.out.print("~");
                 System.out.print(" ");
             }
             System.out.println();
         }
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the coordinates of the Aircraft Carrier (5 cells):");
+        String ship1 = scanner.nextLine();
 
+        for (int i = 0; i < letters.length; i++) {
+            if (ship1.contains(letters[i])) {
+                System.out.println("TEST");
+            }
+
+        }
     }
 }
