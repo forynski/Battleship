e45f9c2bdf4b498b2bc9882101c927bb7b68b22b
forynski <65871510+forynski@users.noreply.github.com>
2021-03-23 16:29:14 +0000

    Update DisplayGridClass.java

--- src/DisplayGridClass.java
+++ src/DisplayGridClass.java
@@ -1,1 +1,19 @@
+package battleship;
+
+public class DisplayGridClass {
+    public static void displayGrid(char[][] grid, char[] alphabets) {
+        System.out.print("  ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.print(alphabets[i] + " ");
+            for (int j = 0; j < 10; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
 
