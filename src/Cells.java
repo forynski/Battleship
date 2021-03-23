e9b577d17365c34dca263e6f8c371e84fd3921c6
forynski <65871510+forynski@users.noreply.github.com>
2021-03-23 10:50:07 +0000

    Created enum for cell status

--- src/Cells.java
+++ src/Cells.java
@@ -1,1 +1,18 @@
+package battleship;
 
+public enum Cells {
+    EMPTY('~'),
+    SHIP('O'),
+    HIT('X'),
+    MISS('M');
+
+    private final char symbol;
+
+    Cells(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
